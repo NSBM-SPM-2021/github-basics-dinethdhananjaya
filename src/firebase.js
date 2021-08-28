608b9ed8ba429e65eb9745cea5bca65dacaffd96
dinethdhananjaya <49784924+dinethdhananjaya@users.noreply.github.com>
2021-08-28 15:14:06 +0530

    first step(add firebase config)

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC3Tb5Ddl1sImD_SQHIQu1Hw196maqwG08",
+    authDomain: "fb-spm.firebaseapp.com",
+    projectId: "fb-spm",
+    storageBucket: "fb-spm.appspot.com",
+    messagingSenderId: "583845690214",
+    appId: "1:583845690214:web:ea5596cc58ff5dcf3f26a7",
+    measurementId: "G-CNXKWC3DK9"
+  };
