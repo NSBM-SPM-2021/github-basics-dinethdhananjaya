7ea21cbbbb0b10f538a7e21813608e868d7391fc
dinethdhananjaya <49784924+dinethdhananjaya@users.noreply.github.com>
2021-09-06 19:53:42 +0530

    StoryReel created

---
+++ src/StoryReel.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Story from './Story';
+import "./StoryReel.css";
+
+function StoryReel() {
+    return (
+        <div className='storyReel'>
+          <Story
+          image="https://neilpatel.com/wp-content/uploads/2017/09/image-editing-tools.jpg"
+          profileSrc="https://pbs.twimg.com/profile_images/1115204219898945537/yULEb58N_400x400.jpg"
+          title="dineth dhananjaya"
+          />
+
+          <Story
+          image="https://neilpatel.com/wp-content/uploads/2017/09/image-editing-tools.jpg"
+          profileSrc="https://pbs.twimg.com/profile_images/1115204219898945537/yULEb58N_400x400.jpg"
+          title="dineth dhananjaya"
+          />
+
+          <Story
+          image="https://neilpatel.com/wp-content/uploads/2017/09/image-editing-tools.jpg"
+          profileSrc="https://pbs.twimg.com/profile_images/1115204219898945537/yULEb58N_400x400.jpg"
+          title="dineth dhananjaya"
+          />
+
+          <Story
+          image="https://neilpatel.com/wp-content/uploads/2017/09/image-editing-tools.jpg"
+          profileSrc="https://pbs.twimg.com/profile_images/1115204219898945537/yULEb58N_400x400.jpg"
+          title="dineth dhananjaya"
+          />
+
+          <Story
+          image="https://neilpatel.com/wp-content/uploads/2017/09/image-editing-tools.jpg"
+          profileSrc="https://pbs.twimg.com/profile_images/1115204219898945537/yULEb58N_400x400.jpg"
+          title="dineth dhananjaya"
+          />
+
+
+         
+            
+        </div>
+    )
+}
+
+export default StoryReel;
