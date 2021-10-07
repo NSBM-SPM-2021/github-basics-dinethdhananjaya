2e4ba691fd2b3f00bad186050e7eddae9e864fbe
dinethdhananjaya <49784924+dinethdhananjaya@users.noreply.github.com>
2021-10-07 20:15:55 +0530

    DB conected and google Auth

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,16 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
 import "./Login.css";
+import { auth, provider} from "./firebase";
 
 function Login() {
     const signIn = () => {
-         //sign in...
+      auth
+         .signInWithPopup(provider)
+         .then((result) => {
+             console.log(result);
+         })
+         .catch((error) => alert(error.message)); 
     };
     return (
         <div className= "login">
@@ -24,6 +30,6 @@ function Login() {
             </Button>
         </div>
     )
-}
+};
 
 export default Login
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "./firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyC3Tb5Ddl1sImD_SQHIQu1Hw196maqwG08",
     authDomain: "fb-spm.firebaseapp.com",
@@ -8,3 +10,13 @@ const firebaseConfig = {
     appId: "1:583845690214:web:ea5596cc58ff5dcf3f26a7",
     measurementId: "G-CNXKWC3DK9"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider(); 
+
+  export {auth, provider};
+  export default db;
+
+ 
